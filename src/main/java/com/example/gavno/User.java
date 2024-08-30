17ba153bb1c091f36b9ec3cc5fce2aa83301549c
glebchik_gg <gaben123001@gmail.com>
2024-08-30 17:16:19 +0300

    db

--- src/main/java/com/example/gavno/User.java
+++ src/main/java/com/example/gavno/User.java
@@ -1,4 +1,21 @@
 package com.example.gavno;
 
-public interface User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String address;
+
+    private boolean isPremium;
+    private boolean isBot;
+    private boolean canJoinGroups;
 }
