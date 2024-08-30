49a714ed9e55602cfd7f116b9b58852be92797e8
glebchik_gg <gaben123001@gmail.com>
2024-08-30 14:05:53 +0300

    a

---
+++ src/test/java/com/example/gavno/GavnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gavno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GavnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
